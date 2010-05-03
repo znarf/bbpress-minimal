ae1a6176cd7a364c73c22d3cef82a17f7526ed3e
François Hodierne <francois@hodierne.net>
2010-05-03 11:33:23 +0200

    fix some non translated strings

--- post-form.php
+++ post-form.php
@@ -1,31 +1,29 @@
-<?php if ( !is_topic() ) : ?>
-<p>
-	<label for="topic"><?php _e('Topic title: (be brief and descriptive)'); ?><br/>
+<?php if ( !bb_is_topic() ) : ?>
+<p id="post-form-title-container">
+	<label for="topic"><?php _e('Title'); ?>
 		<input name="topic" type="text" id="topic" size="50" maxlength="80" tabindex="1" />
 	</label>
 </p>
 <?php endif; do_action( 'post_form_pre_post' ); ?>
-<p>
-	<label for="post_content"><?php _e('Post:'); ?>
+<p id="post-form-post-container">
+	<label for="post_content"><?php _e('Post'); ?>
 		<textarea name="post_content" cols="50" rows="8" id="post_content" tabindex="3"></textarea>
 	</label>
 </p>
-<?php if ( !is_topic() ) : ?>
-<p>
-	<label for="tags-input"><?php printf(__('Enter a few words (called <a href="%s">tags</a>) separated by commas to help someone find your topic:'), bb_get_tag_page_link()) ?>
+<p id="post-form-tags-container">
+	<label for="tags-input"><?php printf(__('Tags (comma seperated)'), bb_get_tag_page_link()) ?>
 		<input id="tags-input" name="tags" type="text" size="50" maxlength="100" value="<?php bb_tag_name(); ?>" tabindex="4" />
 	</label>
 </p>
-<?php endif; ?>
-<?php if ( is_bb_tag() || is_front() ) : ?>
-<p>
-	<label for="forum-id"><?php _e('Pick a section:'); ?>
+<?php if ( bb_is_tag() || bb_is_front() ) : ?>
+<p id="post-form-forum-container">
+	<label for="forum-id"><?php _e('Forum'); ?>
 		<?php bb_new_topic_forum_dropdown(); ?>
 	</label>
 </p>
 <?php endif; ?>
-<p class="submit">
-  <input type="submit" id="postformsub" name="Submit" value="<?php echo attribute_escape( __('Send Post &raquo;') ); ?>" tabindex="4" />
+<p id="post-form-submit-container" class="submit">
+  <input type="submit" id="postformsub" name="Submit" value="<?php echo esc_attr__( 'Send Post &raquo;' ); ?>" tabindex="4" />
 </p>
 
-<p><?php _e('Allowed markup:'); ?> <code><?php allowed_markup(); ?></code>. <br /><?php _e('You can also put code in between backtick ( <code>`</code> ) characters.'); ?></p>
+<p id="post-form-allowed-container" class="allowed"><?php _e('Allowed markup:'); ?> <code><?php allowed_markup(); ?></code>. <br /><?php _e('You can also put code in between backtick ( <code>`</code> ) characters.'); ?></p>
