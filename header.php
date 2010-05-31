29ed394f1f82ec70564c65b4bf4e5e286cb53ed3
François Hodierne <francois@hodierne.net>
2010-05-31 16:53:00 +0200

    h6e_minimal_css utility

--- header.php
+++ header.php
@@ -1,4 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<?php
+function h6e_minimal_css()
+{
+	if (method_exists('Ld_Ui', 'getCssUrl')) {
+		echo Ld_Ui::getCssUrl('/h6e-minimal/h6e-minimal.css', 'css-h6e-minimal');
+	} else if (file_exists(dirname(__FILE__) . '/h6e-minimal')) {
+		echo get_bloginfo('stylesheet_directory') . '/h6e-minimal/h6e-minimal.css';
+	} else if (defined('H6E_CSS')) {
+		echo H6E_CSS . '/h6e-minimal/h6e-minimal.css';
+	} else {
+		echo 'http://h6e.net/css/h6e-minimal/h6e-minimal.css';
+	}
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"<?php bb_language_attributes( '1.1' ); ?>>
 <?php if (is_bb_profile()) : global $self; ?>
 <?php if (!$self) : ?>
@@ -12,7 +25,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php bb_title() ?></title>
 	<?php bb_feed_head(); ?>
-	<link rel="stylesheet" href="http://h6e.net/css/h6e-minimal/h6e-minimal.css" type="text/css" />
+	<link rel="stylesheet" href="<?php h6e_minimal_css() ?>" type="text/css" />
 	<link rel="stylesheet" href="<?php bb_stylesheet_uri(); ?>" type="text/css" />
 <?php if ( 'rtl' == bb_get_option( 'text_direction' ) ) : ?>
 	<link rel="stylesheet" href="<?php bb_stylesheet_uri( 'rtl' ); ?>" type="text/css" />
@@ -35,7 +48,7 @@
 		}
 		?>
 
-		<?php if (class_exists('Ld_Ui') && method_exists('Ld_Ui', 'top_bar') && $top_bar) { Ld_Ui::top_bar(); } else { ?>
+		<?php if (class_exists('Ld_Ui') && method_exists('Ld_Ui', 'top_bar') && $top_bar) { Ld_Ui::topBar(); } else { ?>
 
 			<div class="h6e-simple-header">
 				<?php if ( !in_array( bb_get_location(), array( 'login-page', 'register-page' ) ) ) login_form(); ?>
